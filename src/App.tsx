e83c0babe841bf3959c6a77b98c396dfd5dbba33
evabanegacom <udegbue69@gmail.com>
2024-02-24 01:48:38 +0100

    worked on conditional routing

--- src/App.tsx
+++ src/App.tsx
@@ -18,54 +18,64 @@ import 'react-toastify/dist/ReactToastify.css';
 import Login from './auth/login';
 import ForgotPassword from './auth/forgot-password';
 import ResetPassword from './auth/reset-password';
+import { useSelector } from 'react-redux';
 
 const Combined = () => {
-  return (<>
-  <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Hero />
-      </div>
-    </div>
-    <Welcome />
-  </>
-)
-}
-function App() {
-
+  const isLoggedin = useSelector((state: any) => state?.reducer?.auth?.isAuth);
   return (
-   <div className='bg-primary w-full overflow-hidden'>
-          <ToastContainer />
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Navbar />
+    <>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
       </div>
-    </div>
-
-  
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Combined />} />
-
-        <Route path='/dashboard' element={<Dashboard />}>
-          <Route path='/dashboard/budgets' element={<Budgets />} />
-          <Route path='/dashboard/savings' element={<Savings />} />
-          <Route path='/dashboard/debt-mgts' element={<DebtMgts />} />
-          <Route path='/dashboard/incomes' element={<Incomes />} />
-          <Route path='/dashboard/expenses' element={<Expenses />} />
-          <Route path='/dashboard/overview' element={<Overview />} />
-        </Route>
-       
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/activate' element={<ActivateAccount />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
-        <Route path='/reset-password' element={<ResetPassword />} />
-        <Route path='*' element={<h1>Not Found</h1>} />
-      </Routes>
-    </BrowserRouter>
-    <Footer />
-   </div>
+      <Welcome />
+    </>
   )
 }
 
-export default App
+function App() {
+  const isLoggedin = useSelector((state: any) => state?.reducer?.auth?.isAuth);
+
+  return (
+    <div className='bg-primary w-full overflow-hidden'>
+      <ToastContainer />
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Combined />} />
+          {!isLoggedin && (
+            <>
+              <Route path='/signup' element={<SignUp />} />
+              <Route path='/login' element={<Login />} />
+            </>
+          )}
+          {isLoggedin && (
+            <>
+              <Route path='/dashboard' element={<Dashboard />}>
+                <Route path='/dashboard/budgets' element={<Budgets />} />
+                <Route path='/dashboard/savings' element={<Savings />} />
+                <Route path='/dashboard/debt-mgts' element={<DebtMgts />} />
+                <Route path='/dashboard/incomes' element={<Incomes />} />
+                <Route path='/dashboard/expenses' element={<Expenses />} />
+                <Route path='/dashboard/overview' element={<Overview />} />
+              </Route>
+              <Route path='/activate' element={<ActivateAccount />} />
+              <Route path='/forgot-password' element={<ForgotPassword />} />
+              <Route path='/reset-password' element={<ResetPassword />} />
+            </>
+          )}
+          <Route path='*' element={<h1>Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
